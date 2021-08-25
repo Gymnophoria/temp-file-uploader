457e71af8e7dcb31112016950688929fc5b73ca1
Gymno <buddercreeperman@gmail.com>
2021-08-24 22:05:54 -0700

    add input validation

--- public/index.js
+++ public/index.js
@@ -84,24 +84,110 @@ function dragOverHandler(event) {
 //  file handling
 // ---------------
 
+var fileCount = 0;
 var files = [];
 var fileList = document.getElementById('files');
 
 function handleNewFile(file) {
-    var index = files.length;
+    var id = fileCount++;
     files.push(file);
 
     var li = document.createElement('li');
-    li.setAttribute('id', 'file' + index);
+    li.setAttribute('id', 'file' + id);
     li.innerHTML = '<b>' + file.name + '</b> [<a href="#" onclick="removeFile('
-        + index + ')">remove</a>]';
+        + id + ')">remove</a>]';
 
     fileList.appendChild(li);
 }
 
-function removeFile(index) {
-    files.splice(index, 1);
-    document.getElementById('file' + index).remove();
+function removeFile(id) {
+    files.splice(id, 1);
+    document.getElementById('file' + id).remove();
 }
 
 // https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
+
+// -------------------
+//  upload validation
+// -------------------
+
+var form = document.getElementById('form');
+var expiryQuantity = document.getElementById('expiry-quantity');
+var expiryUnits = document.getElementById('expiry-units');
+var expiryError = document.getElementById('expiry-error');
+var fileSizeError = document.getElementById('filesize-error');
+var noFilesError = document.getElementById('nofiles-error');
+
+var ONE_MINUTE = 60 * 1000;
+var ONE_HOUR = 60 * ONE_MINUTE;
+var ONE_DAY = 24 * ONE_HOUR;
+var ONE_WEEK = 7 * ONE_DAY;
+var MAX_TIME = 2 * ONE_WEEK;
+
+var MAX_FILESIZE = 2000000000; // 2 GB
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+
+    if (checkFiles() && checkTime() && checkFileSize()) {
+        uploadFiles();
+    }
+
+    return false;
+}
+
+function checkTime() {
+    var multiplier;
+
+    switch (expiryUnits.value) {
+        case 'm':
+            multiplier = ONE_MINUTE;
+            break;
+        case 'h':
+            multiplier = ONE_HOUR;
+            break;
+        case 'd':
+            multiplier = ONE_DAY;
+            break;
+        case 'w':
+            multiplier = ONE_WEEK;
+            break;
+        default:
+            return false;
+    }
+
+    return handleError(expiryError, expiryQuantity.value * multiplier > MAX_TIME);
+}
+
+function checkFileSize() {
+    var totalSize = 0;
+
+    for (var i = 0; i < files.length; i++) {
+        totalSize += files[i].size;
+    }
+
+    return handleError(fileSizeError, totalSize > MAX_FILESIZE);
+}
+
+function checkFiles() {
+    return handleError(noFilesError, files.length <= 0);
+}
+
+function handleError(element, hasError) {
+    if (hasError) {
+        element.removeAttribute('hidden');
+        return false;
+    }
+
+    if (!element.hasAttribute('hidden'))
+        element.setAttributeNode(document.createAttribute('hidden'));
+    return true;
+}
+
+// ----------------
+//  file uploading
+// ----------------
+
+function uploadFiles() {
+    console.log('uploading files beep boop bop');
+}
